f1439a79ca451a6b097febcfa12f685acb00b58b
nkrstic98 <nikolakrle998@gmail.com>
2021-06-14 15:51:35 +0200

    Libraries included to Java project; Main function initialized according to project specification;

--- PackageTransportSystem/src/packagetransportsystem/PackageTransportSystem.java
+++ PackageTransportSystem/src/packagetransportsystem/PackageTransportSystem.java
@@ -5,6 +5,10 @@
  */
 package packagetransportsystem;
 
+import rs.etf.sab.operations.*;
+import rs.etf.sab.tests.TestHandler;
+import rs.etf.sab.tests.TestRunner;
+
 /**
  *
  * @author Nikola Krstic
@@ -15,7 +19,26 @@ public class PackageTransportSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        CityOperations cityOperations = null; // Change this to your implementation.
+        DistrictOperations districtOperations = null; // Do it for all classes.
+        CourierOperations courierOperations = null; // e.g. = new MyDistrictOperations();
+        CourierRequestOperation courierRequestOperation = null;
+        GeneralOperations generalOperations = null;
+        UserOperations userOperations = null;
+        VehicleOperations vehicleOperations = null;
+        PackageOperations packageOperations = null;
+
+        TestHandler.createInstance(
+                cityOperations,
+                courierOperations,
+                courierRequestOperation,
+                districtOperations,
+                generalOperations,
+                userOperations,
+                vehicleOperations,
+                packageOperations);
+
+        TestRunner.runTests();
     }
     
 }
